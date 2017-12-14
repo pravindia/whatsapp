52dfbb21e13de000ae7cd890070eb7093d6007a5
Pravin kumar <pravinda4hero@gmail.com>
2017-12-14 19:21:28 +0530

    init

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+{
+	users: [
+		{
+			username: 'admin',
+			tags: [],
+			salt: 'b6239a034b7398fe53a71d5f82fbd5cc',
+			hash: '707b70920086fcbb54c5c866f59009a805e199d8cb873809425f021cea03be98',
+			user_created_timestamp: 1513169901922
+		}
+	]
+}
